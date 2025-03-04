5f5f6afb57fe0a7efafde8581f41f1d244afeb21
Elena Gracheva <ebortova13@gmail.com>
2025-03-04 15:49:19 +0300

    Оптимизация теста (выделение вспомогательных методов и классов) + добавление теста для создания пустой группы

--- test_add_group.py
+++ test_add_group.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from group import Group
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
@@ -27,20 +28,20 @@ class TestAddGroup(unittest.TestCase):
     def open_groups_page(self, wd):
         wd.find_element(By.LINK_TEXT, "groups").click()
 
-    def create_group(self, wd, group_name, group_header, group_footer):
+    def create_group(self, wd, group):
         # init group creation
         wd.find_element(By.NAME, "new").click()
         wd.find_element(By.ID, "content").click()
         # fill group form
         wd.find_element(By.NAME, "group_name").click()
         wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys(group_name)
+        wd.find_element(By.NAME, "group_name").send_keys(group.group_name)
         wd.find_element(By.NAME, "group_header").click()
         wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys(group_header)
+        wd.find_element(By.NAME, "group_header").send_keys(group.group_header)
         wd.find_element(By.NAME, "group_footer").click()
         wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys(group_footer)
+        wd.find_element(By.NAME, "group_footer").send_keys(group.group_footer)
         # submit group creation
         wd.find_element(By.NAME, "submit").click()
 
@@ -57,7 +58,16 @@ class TestAddGroup(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, "admin", "secret")
         self.open_groups_page(wd)
-        self.create_group(wd, "test", "1", "1")
+        self.create_group(wd, Group("test", "1", "1"))
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, "admin", "secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, Group("", "", ""))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
